bba15929a1aff638b0a268c0770ba990ded7298b
Steven <stevengf14@gmail.com>
2022-10-26 22:49:48 -0500

    Starting from Scratch

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders without crashing', () => {
-  render(<App />);
-  const div = screen.getByText(/Hi there/i);
-  expect(div).toBeInTheDocument();
-});
