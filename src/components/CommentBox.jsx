f4dff57ba3aceafece4609afa3110b187615252d
Steven <stevengf14@gmail.com>
2022-11-02 22:51:19 -0500

    CommentBox

--- src/components/CommentBox.jsx
+++ src/components/CommentBox.jsx
@@ -1,5 +1,29 @@
 import React from "react";
+import { useState } from "react";
 
 export default function CommentBox() {
-  return <div>Comment Box</div>;
+  const [comment, setComment] = useState("");
+
+  const handleChange = (event) => {
+    setComment(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // TODO: Call an action creator
+    // And save the comment
+
+    setComment("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h4>Add a Comment</h4>
+      <textarea onChange={handleChange} value={comment} />
+      <div>
+        <button>Submit Comment</button>
+      </div>
+    </form>
+  );
 }
