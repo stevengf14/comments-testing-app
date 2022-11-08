482259b7c3363125df1eb44c246f8f289bce5d67
Steven <stevengf14@gmail.com>
2022-11-07 22:19:19 -0500

    form submit excercise

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -9,7 +9,7 @@ beforeEach(() => {
 });
 
 afterEach(() => {
-    wrapped.unmount();
+  wrapped.unmount();
 });
 
 it("has a text area and a button", () => {
@@ -18,7 +18,20 @@ it("has a text area and a button", () => {
 });
 
 it("has a text area that users can type in", () => {
-  wrapped.find('textarea').simulate("change", {
-    target: {value: 'new comment'}
+  wrapped.find("textarea").simulate("change", {
+    target: { value: "new comment" },
   });
-})
+  wrapped.update();
+
+  expect(wrapped.find("textarea").prop("value")).toEqual("new comment");
+});
+
+it("when form is submitted, text area gets emptied", () => {
+  wrapped.find("textarea").simulate("change", {
+    target: { value: "new comment" },
+  });
+  wrapped.update();
+  wrapped.find("form").simulate("submit");
+  wrapped.update();
+  expect(wrapped.find("textarea").prop("value")).toEqual("");
+});
