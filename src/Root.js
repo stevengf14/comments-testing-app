aaef58ee7b9dd1f50679ea9b5d9af75c02735348
Steven <stevengf14@gmail.com>
2022-11-24 22:40:38 -0500

    State validation middleware

--- src/Root.js
+++ src/Root.js
@@ -2,11 +2,15 @@ import React from "react";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import async from "./middlewares/async";
-import reduxPromise from "redux-promise";
+import stateValidator from "./middlewares/stateValidator";
 import reducers from "./reducers";
 
 const Root = ({ children, initialState = {} }) => {
-  const store = createStore(reducers, initialState, applyMiddleware(async));
+  const store = createStore(
+    reducers,
+    initialState,
+    applyMiddleware(async, stateValidator)
+  );
   return <Provider store={store}>{children}</Provider>;
 };
 export default Root;
